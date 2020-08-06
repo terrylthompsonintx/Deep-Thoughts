2af1275d782585810722202e003805803f59f86d
Terry Thompson <terrylthompsonintx@gmail.com>
2020-08-06 18:07:21 -0500

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,13 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const client = new ApolloClient({
   uri: '/graphql'
@@ -13,13 +18,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
       <div className="flex-column justify-flex-start min-100-vh">
+
         <Header />
         <div className="container">
-          <Home />
+        <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/thought" component={SingleThought} />
+            <Route exact path="/profile/:username?" component={Profile} />
+            <Route exact path="/thought/:id" component={SingleThought} />
+            <Route component={NoMatch} />
         </div>
         <Footer />
       </div>
+      </Router>
     </ApolloProvider>
   );
 }
